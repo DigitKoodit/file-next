ca4be2e715918e9829ea060af1e4f3da64c553ed
Tatu Heinonen <heinonen.tatu@gmail.com>
2020-03-14 19:10:09 +0200

    Working on CSS fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-const withTypescript = require('@zeit/next-typescript')
+const withTypescript = require('@zeit/next-typescript');
+const withCSS = require('@zeit/next-css');
 const withOffline = require('next-offline');
 const webpack = require('webpack');
 require('dotenv').config();
 
-module.exports = withTypescript(withOffline({
+module.exports = withTypescript(withCSS(withOffline({
   webpack: config => {
     const env = Object.keys(process.env).reduce((acc, curr) => {
       acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
@@ -13,4 +14,4 @@ module.exports = withTypescript(withOffline({
     config.plugins.push(new webpack.DefinePlugin(env));
     return config;
   }
-}));
+})));
