848999986ba6edd84455b044853e2dcb6152e64c
Tatu Heinonen <tatahe@utu.fi>
2020-03-16 12:29:16 +0200

    Working on CSS fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,3 @@
-import '../static/emptycss';
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
@@ -10,27 +9,18 @@ interface props {
 
 // Document component is strongly typed with `@types/next`
 export default class MyDocument extends Document<props> {
-  static async getInitialProps(ctx) {
+  static getInitialProps({ renderPage }: any) {
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-    
     try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-        });
-      const initialProps = await Document.getInitialProps(ctx);
-
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
-      };
-    } finally {
+      const page = renderPage(App => props =>
+        sheet.collectStyles(<App {...props} />)
+      );
+      const styleTags = sheet.getStyleElement();
+      return { ...page, styleTags };
+    } catch (error) {
+      console.log(error);
+    }
+    finally {
       sheet.seal();
     }
   }
@@ -39,7 +29,7 @@ export default class MyDocument extends Document<props> {
     return (
       <Html lang="en">
         <Head>
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Quattrocento&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css?family=Open+Sans|Quattrocento&display=swap" rel="stylesheet"/>
           {this.props.styleTags}
         </Head>
         <Body>
